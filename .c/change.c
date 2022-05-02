9ead801d282277c79ff13ecbbcd852875835f90d
wkwbk <778057151@qq.com>
2022-05-02 17:01:09 +0800

    2022年5月2日17点01分

--- .c/change.c
+++ .c/change.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int amount=0;
+	//初始化
+	int bill=0;
 	int price=0;
-	
+	//读入金额和票面
 	printf("请输入金额（元）：");
 	scanf("%d",&price);
-	
 	printf("请输入票面（元）：");
-	scanf("%d",&amount);
-	
-	int change=amount-price;
-	
-	printf("找您%d元\n",change);
-	
-	return 0;
+	scanf("%d",&bill);
+	//计算找零
+	if(bill>=price){
+		printf("找您%d元\n",bill-price);//TODO
+	} else{
+		printf("您还需要付%d\n",price-bill);
+	}	
 }
--- .c/relational-operation-result.c
+++ .c/relational-operation-result.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	printf("%d\n",5==3);
-	printf("%d\n",5>3);
-	printf("%d\n",5<=3);
+	printf("%d\n",5==3); //不成立，结果为0
+	printf("%d\n",5>3); //成立，结果为1
+	printf("%d\n",5<=3); //不成立，结果为0
 }
